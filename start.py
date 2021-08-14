35f8ba37ad9ca2b67f21bf7dbc2cfd34feace092
Gabriel Rosalino <gabriel.frosalino@gmail.com>
2021-08-14 14:50:06 -0300

    config: initial environment

---
+++ start.py
@@ -0,0 +1,7 @@
+def soma(num_1: int, num_2: int) -> int:
+    """Soma dois números
+    :param - num_1: primeiro número da soma
+            - num_2: segundo número da soma
+    """
+    soma_dois_numeros = num_1 + num_2
+    return soma_dois_numeros
